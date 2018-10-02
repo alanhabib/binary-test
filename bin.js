05bd7fcbc1338fbdf64aa18f3b8cf0fd1c741e3b
Alan Habib <ahabib@centuri.local>
2018-10-02 11:23:47 +0200

    changed the bin.js file now it should go through the whole binary file and then take out targetCount, there is problem with dynamicCount

--- bin.js
+++ bin.js
@@ -1,19 +1,9 @@
 const fs = require("fs");
 
-fs.readFile("./trotting.bin", (err, data) => {
-  if (err) throw err;
+// fs.readFile("./trotting.bin", (err, data) => {
+//   if (err) throw err;
 
-  // exempel på hur man kan kopiera alla värden över
-  // från bufferten till en annan array
-  let curOffset = 0;
-  let myBufferData = [];
-  while (curOffset < data.byteLength) {
-    myBufferData.push(data.readInt8(curOffset));
-    curOffset += 1;
-  }
-
-  console.log(myBufferData);
-});
+// kopiera alla värden över från bufferten till en annan array
 
 fs.readFile("./trotting.bin", (err, data) => {
   if (err) throw err;
@@ -21,9 +11,88 @@ fs.readFile("./trotting.bin", (err, data) => {
   // 1 byte = 8 bitar = 2 hex
   //console.log(data);
 
+  // här tar jag ut packetheader för att ta mig igenom den
+  //  i samma veva försöker jag ta ut targetcount från targetheader för att få ut mina hästar
+
+  const packet = packetHeader(data);
+
+  let targetCounter = 0, offset = packet.offset;
+
+  while (offset < data.length && targetCounter < packet.targetCount) {
+    const block = data.slice(offset, offset + 25);
+    console.log(targetCounter, offset, '-------------------------------------');
+    targetHeader(block);
+    offset += block.length
+    targetCounter++;
+  }
+
+  // för att se ifall något körs efter att vi utfört vår while loop ovan
+  // const leftPacket = [];
+  // while (offset < data.length) {
+  //   leftPacket.push(data.readInt8(offset));
+  //   offset += 1;
+  // }
+  // console.log(leftPacket);
+
+
+  // Ifall man väljer att lägga in dynamic data kör man det på det här sättet
+
+  // Dynamic Data Package
+  if (packet.dynamicDataCount > 0) {
+    const dynamicDataId = data.readInt8(offset);
+    offset += 1;
+    const dynamicDataSize = data.readInt32LE(offset);
+    offset += 4;
+    const dynamicData = data[offset, offset + packet.dynamicDataSize];
+    offset += dynamicDataSize;
+
+    console.log("dynamicDataId: " + dynamicDataId);
+    console.log("dynamicDataSize: " + packet.dynamicDataSize);
+    console.log("dynamicData: " + dynamicData);
+  };
+});
+
+// bryter ut target header till en egen funktion  och tar ut datan från varje element
+
+function targetHeader(data) {
   let curOffset = 0;
 
-  // Packet header
+  const horseNo = data.readInt8(curOffset);
+  curOffset += 1;
+
+  const posX = data.readFloatLE(curOffset); // 22 (BE)
+  curOffset += 4;
+
+  const posY = data.readFloatLE(curOffset); // 26 (BE)
+  curOffset += 4;
+
+  const posZ = data.readFloatLE(curOffset); // 30 (BE)
+  curOffset += 4;
+
+  const laneNumber = data.readFloatLE(curOffset); // 34 (BE)
+  curOffset += 4;
+
+  const distanceToGoalLine = data.readFloatLE(curOffset); // 38 (BE)
+  curOffset += 4;
+
+  const speed = data.readFloatLE(curOffset); // 42 (BE)
+  curOffset += 4;
+
+  console.log("horseNo: " + horseNo);
+  console.log("posX: " + posX);
+  console.log("posY: " + posY);
+  console.log("posZ: " + posZ);
+  console.log("laneNumber: " + laneNumber);
+  console.log("distanceToGoalLine: " + distanceToGoalLine);
+  console.log("speed: " + speed);
+
+}
+
+// lägger packet header i egen funktion och försöker utvinna datan från det
+// Packet header
+function packetHeader(data) {
+  let curOffset = 0;
+
   const versionId = data.readInt8(curOffset);
   curOffset += 1;
 
@@ -55,47 +124,17 @@ fs.readFile("./trotting.bin", (err, data) => {
   console.log("dynamicDataSize: " + dynamicDataSize);
   console.log("dynamicDataCount: " + dynamicDataCount);
 
-  // Target header
-  const horseNo = data.readInt8(curOffset);
-  curOffset += 1;
+  // lägga in detta i object för att få ut targetCount och offset
+  return {
+    versionId: versionId,
+    trackId: trackId,
+    raceId: raceId,
+    timestamp: timestamp,
+    targetCount: targetCount,
+    dynamicDataSize: dynamicDataSize,
+    dynamicDataCount: dynamicDataCount,
+    offset: curOffset
+  };
+}
 
-  const posX = data.readFloatLE(curOffset); // 22 (BE)
-  curOffset += 4;
 
-  const posY = data.readFloatLE(curOffset); // 26 (BE)
-  curOffset += 4;
-
-  const posZ = data.readFloatLE(curOffset); // 30 (BE)
-  curOffset += 4;
-
-  const laneNumber = data.readFloatLE(curOffset); // 34 (BE)
-  curOffset += 4;
-
-  const distanceToGoalLine = data.readFloatLE(curOffset); // 38 (BE)
-  curOffset += 4;
-
-  const speed = data.readFloatLE(curOffset); // 42 (BE)
-  curOffset += 4;
-
-  console.log("horseNo: " + horseNo);
-  console.log("posX: " + posX);
-  console.log("posY: " + posY);
-  console.log("posZ: " + posZ);
-  console.log("laneNumber: " + laneNumber);
-  console.log("distanceToGoalLine: " + distanceToGoalLine);
-  console.log("speed: " + speed);
-
-  // Dynamic Data Package
-  // if(dynamicDataCount > 0) {
-  //     const dynamicDataId = data.readInt8(curOffset);
-  //     curOffset += 1;
-  //     const dynamicDataSize = data.readInt32LE(curOffset);
-  //     curOffset += 4;
-  //     const dynamicData = data[curOffset, curOffset + dynamicDataSize];
-  //     curOffset += dynamicDataSize;
-
-  //     console.log("dynamicDataId: " + dynamicDataId);
-  //     console.log("dynamicDataSize: " + dynamicDataSize);
-  //     console.log("dynamicData: " + dynamicData);
-  //     };
-});
